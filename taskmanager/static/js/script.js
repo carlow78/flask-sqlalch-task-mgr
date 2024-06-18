b04b76a0227dd6495c455e9e9a1d6f6163d529fa
John Lynch <johnf.lynch@yahoo.ie>
2024-06-18 17:40:25 +0000

    Add task feature based on add category

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,15 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
+
+    //datepicker intialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n:{done: "Select"}
+    });
+    //select intialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+
   });
